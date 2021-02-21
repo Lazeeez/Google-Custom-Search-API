6634bc95fa85e62d6298ac5ef0752e6471ddcf46
Lajat Manekar <64376519+lajatManekar22@users.noreply.github.com>
2021-02-21 23:06:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Customsearch.v1.Data;
+
+namespace TestGoogleSearch
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        string query = "https://en.wikipedia.org/wiki/Dior";
+
+        var results = GoogleSearch.Search(query);
+            foreach (Result result in results)
+        {
+                Console.WriteLine();
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine(result.Snippet);
+            }
+        Console.ReadKey();
+    }
+}
+}
